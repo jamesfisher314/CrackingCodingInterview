898f7d4d94b46e2c22032386cc1de9ec561d15f8
James Fisher <Fisher.James@outlook.com>
2017-02-10 19:49:26 -0800

    add code from paper in project 2.7

---
+++ 02.07 Linked List Intersection/02.07 Linked List Intersection.cpp
@@ -0,0 +1,38 @@
+// 02.07 Linked List Intersection.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+class Node {
+	Node *next;
+	int value;
+};
+
+Node *Intersection(Node*, Node*);
+int GetLengthofLinkedList(Node*);
+void swap(int, int);
+
+int main()
+{
+    return 0;
+}
+
+Node *Intersection(Node *head1, Node *head2) {
+	int length1 = GetLengthofLinkedList(*head1);
+	int length2 = GetLengthofLinkedList(*head2); // check for zero length here
+
+	if (length2 < length1)
+		swap(length1, length2); // can I directly change stack values? // only if I pass reference
+
+	while (length1 < length2--)
+		*head2 = head2::next; // how does one move a pointer?
+
+	if (*head1 == *head2)
+		return *head1;
+
+	while (head1::next != head2::next) { // same length, no NRE b/c null == null at end
+		*head1 = head1::next;
+		*head2 = head2::next;
+	}
+	return *head1::next;
+};
