fa0295a82036310f387315781888127d0063373b
James Fisher <Fisher.James@outlook.com>
2017-02-28 12:03:38 -0800

    Add c# console project for Sparse Similarity

---
+++ 17.26 Sparse Document Similarity/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _17._26_Sparse_Document_Similarity
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
