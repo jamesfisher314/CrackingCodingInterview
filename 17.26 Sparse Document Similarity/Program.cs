2f854a80451e9b8cee9fbdb7fc9f8920ecbfc872
James Fisher <Fisher.James@outlook.com>
2017-02-28 13:15:38 -0800

    17.26: Theoretically complete implementation. However, a blank c# console app doesn't run.

--- 17.26 Sparse Document Similarity/Program.cs
+++ 17.26 Sparse Document Similarity/Program.cs
@@ -3,17 +3,36 @@ using System.Collections.Generic;
 
 namespace _17._26_Sparse_Document_Similarity
 {
-	public class Program
+	public static class Program
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			IList<IList<int>> inputDocs = new List<IList<int>>() {
+				null,									// 0
+				null,									// 1
+				null,									// 2
+				new List<int> { 14, 15, 100, 9, 3 },	// 3
+				new List<int>(),						// 4
+				new List<int>{314159265},				// 5
+				new List<int>{32, 1, 9, 3, 5 },			// 6
+				new List<int>(),						// 7
+				null,									// 8
+				new List<int>{15, 29, 2, 6, 8, 7 },		// 9
+				null,									//10
+				null,									//11
+				null,									//12
+				null,									//13
+				new List<int>{7, 10 }					//14
+			};
+			IDictionary<int, IList<int>> haveThisWord = ReadIntoMatches(inputDocs);
+			Similarity(haveThisWord, inputDocs);
+			Console.ReadLine();
 		}
 
-		public void Similarity(Dictionary<int, IList<int>> haveThisWord, IList<IList<int>> docs)
+		public static void Similarity(IDictionary<int, IList<int>> haveThisWord, IList<IList<int>> docs)
 		{
 			PrintHeader();
-			var matches = new Dictionary<int, ISet<int>>();
+			IDictionary<int, ISet<int>> matches = new Dictionary<int, ISet<int>>();
 			for (var i = 0; i < haveThisWord.Keys.Count; i++)
 			{
 				var match = haveThisWord[i];
@@ -23,7 +42,7 @@ namespace _17._26_Sparse_Document_Similarity
 				{
 					for (var n = m + 1; n < match.Count; n++)
 					{
-						AddToMatches(ref matches, m, n);
+						AddToMatches(ref matches, match[m], match[n]);
 					}
 				}
 			}
@@ -34,23 +53,52 @@ namespace _17._26_Sparse_Document_Similarity
 			}
 		}
 
-		private void AddToMatches(ref Dictionary<int, ISet<int>> matches, int m, int n)
+		private static IDictionary<int, IList<int>> ReadIntoMatches(IList<IList<int>> inputDocs)
 		{
-			throw new NotImplementedException();
+			IDictionary<int, IList<int>> hasThisWord = new Dictionary<int, IList<int>>();
+			for (var i = 0; i < inputDocs.Count; i++)
+			{
+				if (inputDocs[i] == null || inputDocs[i].Count == 0)
+					continue;
+				foreach (var word in inputDocs[i])
+				{
+					AddToMapDictionary(ref hasThisWord, word, i);
+				}
+			}
+			return hasThisWord;
 		}
 
-		private void PrintHeader()
+		private static void AddToMapDictionary(ref IDictionary<int, IList<int>> hasThisWord, int word, int i)
+		{
+			if (!hasThisWord.ContainsKey(word))
+				hasThisWord.Add(word, new List<int>(i));
+			else
+				hasThisWord[word].Add(i);
+		}
+
+		private static void AddToMatches(ref IDictionary<int, ISet<int>> matches, int m, int n)
+		{
+			if (!matches.ContainsKey(m))
+				matches.Add(m, new HashSet<int> { n });
+			else
+				matches[m].Add(n);
+		}
+
+		private static void PrintHeader()
 		{
 			Console.WriteLine("ID1, ID2 \t: SIMILARITY");
 		}
 
-		public void PrintSimilarity(IList<IList<int>> docs, int doc, int match)
+		public static void PrintSimilarity(IList<IList<int>> docs, int doc, int match)
 		{
 			var total = docs[doc].Count + docs[match].Count;
 			var equal = 0;
 			foreach (var word in docs[doc])
-				if (docs[match].Contains(word))
+			{
+				var matchWords = docs[match];
+				if (matchWords.Contains(word))
 					equal++;
+			}
 			Console.WriteLine(doc + ", " + match + "\t: " + (equal / (total - equal)));
 		}
 	}
