672eb8ab9398685da56df54ab15beb18875036fc
James Fisher <Fisher.James@outlook.com>
2017-03-02 13:30:05 -0800

    SESC: implement IsValidInput

--- SESCDistance/SESCDistance.cs
+++ SESCDistance/SESCDistance.cs
@@ -7,7 +7,14 @@ namespace SESCDistance
 	{
 		public static IDictionary<double, IList<IList<int>>> LegalDistanceRoutes(IList<double> distances, int mustVisit)
 		{
+			if (!IsValidInput(distances, mustVisit))
+				return default(IDictionary<double, IList<IList<int>>>);
 			throw new NotImplementedException();
+		}
+
+		private static bool IsValidInput(IList<double> distances, int mustVisit)
+		{
+			return distances == null || distances.Count < 1 || distances.Count <= mustVisit;
 		}
 	}
 }
