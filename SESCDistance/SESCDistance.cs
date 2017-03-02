f440f491516b9b8a1c9274c729e53381240024bb
James Fisher <Fisher.James@outlook.com>
2017-03-02 13:48:32 -0800

    SESC: Implement logic for 010 route when mustVisit allows

--- SESCDistance/SESCDistance.cs
+++ SESCDistance/SESCDistance.cs
@@ -9,7 +9,11 @@ namespace SESCDistance
 		{
 			if (!IsValidInput(distances, mustVisit))
 				return default(IDictionary<double, IList<IList<int>>>);
-			throw new NotImplementedException();
+
+			IDictionary<double, IList<IList<int>>> lengthRoute = new Dictionary<double, IList<IList<int>>>();
+			if (mustVisit <= 1)
+				lengthRoute.Add(2 * distances[0], new List<IList<int>> { new List<int> { 1 } });
+			return lengthRoute;
 		}
 
 		private static bool IsValidInput(IList<double> distances, int mustVisit)
