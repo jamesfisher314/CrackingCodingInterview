408836f2945633426f3eccd4506cc3ceabc571f7
James Fisher <fisher.james@outlook.com>
2017-03-02 09:15:42 -0800

    Add test class to test SESC distance

---
+++ CSharp Tests/SESCDistance.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharp_Tests
+{
+	[TestClass]
+	class SESCDistance
+	{
+		[TestMethod]
+		public void SESCCanonical()
+		{
+
+		}
+	}
+}
