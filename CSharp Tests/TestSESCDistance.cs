0efa29b77d775cc72df782c711148de6e099a536
James Fisher <Fisher.James@outlook.com>
2017-03-03 11:03:44 -0800

    SESC: Revise max distance estimation to use index-based formula

--- CSharp Tests/TestSESCDistance.cs
+++ CSharp Tests/TestSESCDistance.cs
@@ -9,16 +9,19 @@ namespace CSharp_Tests
 	public class TestSESCDistance
 	{
 		[TestMethod]
-		public void SESCCanonical()
+		public void SESCCanonicalEven()
 		{
 			IList<double> distances = new List<double> { 0.1, 8, 23.77, 3.1415926, 0.64, 17 };
 			var mustVisit = 5;
 
 			IDictionary<double, IList<IList<int>>> LegalRoutes = SESCDistance.SESCDistance.LegalDistanceRoutes(distances, mustVisit);
 			Assert.IsTrue(LegalRoutes.Count > 2);
-			var shouldBeMax = Math.Round(2 * (distances[0] + distances[5]) + 4 * (distances[1] + distances[4]) + 6 * (distances[2] + distances[3]), 8);
+			var shouldBeMax = 0.0;
+			var i = 0;
+			for (; i < distances.Count / 2; i++)
+				shouldBeMax += 2 * (i + 1) * (distances[i] + distances[distances.Count - i - 1]);
 			var isMax = Math.Round(LegalRoutes.Keys.Max(), 8);
-			Assert.AreEqual(shouldBeMax, isMax);
+			Assert.AreEqual(Math.Round(shouldBeMax, 8), isMax);
 		}
 
 		[TestMethod]
