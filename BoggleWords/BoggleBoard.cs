e2de25ab2c9128b63db75c0c0eef4f3ea5397e32
James Fisher <Fisher.James@outlook.com>
2017-03-07 17:58:35 -0800

    Boggle: Add rudimentary test an class for boggle word count

---
+++ BoggleWords/BoggleBoard.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BoggleWords
+{
+	public class BoggleBoard
+	{
+		public BoggleBoard(int size)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
---
+++ CSharp Tests/TestBoggle.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharp_Tests
+{
+	[TestClass]
+	class TestBoggle
+	{
+		[TestMethod]
+		public void BoggleAccessLetters()
+		{
+			var board = new BoggleWords.BoggleBoard(3);
+		}
+	}
+}
