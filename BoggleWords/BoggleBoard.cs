604b093e29694409812bc5e1de46e5f556fd0092
James Fisher <Fisher.James@outlook.com>
2017-03-07 18:10:08 -0800

    Boggle: Test for accessing characters in Boggle board and empty methods to support

--- BoggleWords/BoggleBoard.cs
+++ BoggleWords/BoggleBoard.cs
@@ -1,10 +1,21 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace BoggleWords
 {
 	public class BoggleBoard
 	{
 		public BoggleBoard(int size)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void Initialize(IList<char> chars)
+		{
+			throw new NotImplementedException();
+		}
+
+		public char GetChar(int x, int y)
 		{
 			throw new NotImplementedException();
 		}
--- CSharp Tests/TestBoggle.cs
+++ CSharp Tests/TestBoggle.cs
@@ -12,6 +12,12 @@ namespace CSharp_Tests
 		public void BoggleAccessLetters()
 		{
 			var board = new BoggleWords.BoggleBoard(3);
+			var chars = new List<char> { 'h', 'e', 'l', 't', 'o', 'l', 'a', 'c', 'c' };
+			board.Initialize(chars);
+			for (var i = 0; i < chars.Count; i++)
+			{
+				Assert.AreEqual(chars[i], board.GetChar(i / 3, i % 3));
+			}
 		}
 	}
 }
