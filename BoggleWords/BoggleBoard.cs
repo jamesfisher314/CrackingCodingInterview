36d498c3ce3a6cbc407c9a5c7169aa1122091f00
James Fisher <Fisher.James@outlook.com>
2017-03-08 13:16:35 -0800

    Boggle: Implement exceptions

--- BoggleWords/BoggleBoard.cs
+++ BoggleWords/BoggleBoard.cs
@@ -11,17 +11,31 @@ namespace BoggleWords
 
 		public BoggleBoard(int size)
 		{
+			if (size < 2)
+				throw new ArgumentException("Size must be at least two.", nameof(size));
 			Size = size;
+			board = new char[0];
 		}
 
 		public void Initialize(IEnumerable<char> chars)
 		{
-			board = chars.ToArray();
+			if (chars == default(IEnumerable<char>) || !chars.Any())
+				throw new ArgumentException("Must provide characters.", nameof(chars));
+			var charInput = chars.ToArray();
+			if (charInput.Length != Size * Size)
+				throw new ArgumentException("Must provide " + nameof(Size) + " ^ 2 characters.");
+			board = charInput;
 		}
 
 		public char GetChar(int x, int y)
 		{
-			return board[x * 3 + y];
+			if (board.Length == 0)
+				throw new InvalidOperationException(nameof(board) + " must be initialized.");
+			if (x >= Size || y >= Size)
+				throw new IndexOutOfRangeException(nameof(x) + " and " + nameof(y) + " are not in the existing board.");
+
+			var index = x * 3 + y;
+			return board[index];
 		}
 	}
 }
