34245dfa30bec79c432621cd36c3f94f2dbdcf38
James Fisher <Fisher.James@outlook.com>
2017-03-14 10:31:35 -0700

    Dict: implement prefix processing

--- Dictionary/English.cs
+++ Dictionary/English.cs
@@ -23,11 +23,27 @@ namespace Dict
 				{
 					wordSet = new HashSet<string>();
 					foreach (var word in Source)
-						wordSet.Add(word);
+						wordSet.Add(word.ToLowerInvariant());
 				}
 				return wordSet;
 			}
 		}
+
+		static TrieNode leadsTo = null;
+		static TrieNode LeadsTo {
+			get {
+				if (leadsTo == null)
+				{
+					leadsTo = new TrieNode();
+					foreach (var word in WordSet)
+					{
+						leadsTo.Add(word);
+					}
+				}
+				return leadsTo;
+			}
+		}
+
 		public static int Count { get { return WordSet.Count; } }
 
 		public readonly int SubCount;
@@ -48,14 +64,55 @@ namespace Dict
 
 		public static bool Contains(string word)
 		{
-			return WordSet.Contains(word);
+			return WordSet.Contains(word.ToLowerInvariant());
 		}
 
 		public static bool LeadsToWord(string prefix)
 		{
-			throw new NotImplementedException();
+			return LeadsTo.Contains(prefix);
+		}
+	}
+
+	class TrieNode
+	{
+		public static char EndValue = '\0';
+		IDictionary<char, TrieNode> children = new Dictionary<char, TrieNode>();
+		internal void Add(string suffix)
+		{
+			string firstString = null;
+			if (string.IsNullOrWhiteSpace(suffix)) {
+				children.Add(EndValue, new TrieNode());
+				return;
+			}
+			firstString = suffix?.Substring(0, 1);
+			if (string.IsNullOrWhiteSpace(firstString)){ 
+				children.Add(EndValue, new TrieNode());
+				return;
+			}
+
+			TrieNode next;
+			var firstChar = firstString.ToCharArray()[0];
+			if (children.ContainsKey(firstChar))
+				next = children[firstChar];
+			else {
+				next = new TrieNode();
+				children.Add(firstChar, next);
+			}
+			if (suffix.Length > 1)
+				next.Add(suffix.Substring(1));
 		}
 
-		
+		internal bool Contains(string prefix)
+		{
+			var firstString = prefix?.Substring(0, 1);
+			if (string.IsNullOrEmpty(prefix) || string.IsNullOrWhiteSpace(firstString))
+				return children.ContainsKey(EndValue);
+
+			var firstChar = firstString.ToCharArray()[0];
+			var canProceed = children.ContainsKey(firstChar);
+			if (canProceed && prefix.Length > 1)
+				return children[firstChar].Contains(prefix.Substring(1));
+			return canProceed;
+		}
 	}
 }
