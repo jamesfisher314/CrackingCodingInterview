7a102c3ef2e1eaa4482eb25f63dceb7b04e9fe86
James Fisher <Fisher.James@outlook.com>
2017-03-14 08:28:11 -0700

    17.25: Add a dictionary because it will be useful in multiple projects

---
+++ CSharp Tests/TestDict.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharp_Tests
+{
+	[TestClass]
+	public class TestDict
+	{
+		[TestMethod]
+		public void DictContainsWords()
+		{
+			Dict.EN.file = "wordsEn.txt";
+			Assert.IsTrue(Dict.EN.Contains("cat"));
+			Assert.IsFalse(Dict.EN.Contains("blorffle"));
+			Assert.IsFalse(Dict.EN.Contains("1"));
+			Assert.AreEqual(109583, Dict.EN.Count);
+		}
+
+		[TestMethod]
+		public void DictPrefixes()
+		{
+			Assert.IsTrue(Dict.EN.LeadsToWord("ca"));
+			Assert.IsTrue(Dict.EN.LeadsToWord("cat"));
+			Assert.IsFalse(Dict.EN.LeadsToWord("blor"));
+		}
+
+		[TestMethod]
+		public void DictWordLength()
+		{
+			Assert.AreEqual(4030, new Dict.EN(4).SubCount);
+		}
+	}
+}
---
+++ Dictionary/English.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Dict
+{
+	public class EN
+	{
+		public static string file = "ENWords.csv";
+		static string[] source = null;
+		static string[] Source {
+			get {
+				if (source == null)
+					source = File.ReadAllLines(file);
+				return source;
+			}
+		}
+
+		static HashSet<string> wordSet = null;
+		static ISet<string> WordSet {
+			get {
+				if (wordSet == null)
+				{
+					wordSet = new HashSet<string>();
+					foreach (var word in Source)
+						wordSet.Add(word);
+				}
+				return wordSet;
+			}
+		}
+		public static int Count { get { return WordSet.Count; } }
+
+		public readonly int SubCount;
+		public EN(int wordLength)
+		{
+			throw new NotImplementedException();
+		}
+
+		public bool SubDictContains(string word)
+		{
+			throw new NotImplementedException();
+		}
+
+		public bool SubDictLeadsToWord(string prefix)
+		{
+			throw new NotImplementedException();
+		}
+
+		public static bool Contains(string word)
+		{
+			return WordSet.Contains(word);
+		}
+
+		public static bool LeadsToWord(string prefix)
+		{
+			throw new NotImplementedException();
+		}
+
+		
+	}
+}
