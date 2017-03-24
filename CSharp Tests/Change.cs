085d21f4c950eabd3bf2b4b86731e4d1caaf6515
James Fisher <fisher.james@outlook.com>
2017-03-24 15:22:50 -0700

    Change: Add project and test class

---
+++ CSharp Tests/Change.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharp_Tests
+{
+	[TestClass]
+	public class Change
+	{
+		[TestMethod]
+		public void ChangeZero()
+		{
+			Assert.Fail();
+		}
+	}
+}
