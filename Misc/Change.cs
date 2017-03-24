c7cd73bb5c19a0ba540150c9c27a78a53c55bdcc
James Fisher <fisher.james@outlook.com>
2017-03-24 16:21:19 -0700

    Change: Greenfactor to plausible full logic

--- Misc/Change.cs
+++ Misc/Change.cs
@@ -1,17 +1,40 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Misc
 {
 	public class Change
 	{
-		public static int CountOf(int cents)
+		private static readonly IDictionary<int, IEnumerable<int>> CoinOptions = 
+			new Dictionary<int, IEnumerable<int>> {
+				{ 25, new List<int> { 25, 10, 5, 1 } },
+				{ 10, new List<int> { 10, 5, 1} },
+				{  5, new List<int> { 5, 1} },
+				{  1, new List<int> {1 } },
+			};
+
+		public static int CountOf(int cents, IEnumerable<int> coins = default(IEnumerable<int>))
 		{
 			if (cents < 0)
 				throw new ArgumentOutOfRangeException(nameof(cents), "Use this method for positive values owed");
 			if (cents <= 1)
 				return cents;
 
-			return CountOf(cents - 1);
+			var total = 0;
+			if (coins == default(IEnumerable<int>))
+				coins = CoinOptions[25];
+
+			foreach (var coin in coins)
+			{
+				if (coin <= cents)
+				{
+					total += CountOf(cents - coin, CoinOptions[coin]);
+					if (coin == cents)
+						total++;
+				}
+			}
+
+			return total;
 		}
 	}
 }
